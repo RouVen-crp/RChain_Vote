49a6f053d8b46c06b6954c4fcd329bf21f5e3132
RouVen-crp <rou_ven@163.com>
2023-11-15 22:23:18 +0800

    Colorset changed & background img(uncompleted)

--- src/views/LogAndReg.vue
+++ src/views/LogAndReg.vue
@@ -2,7 +2,7 @@
     <!-- <HelloWorld /> -->
     <LoginAndRegister />
   </template>
-  
+
   <script setup>
     import mainpage from '@/components/mainpage.vue'
     import LoginAndRegister from '@/components/LoginAndRegister.vue'
