0ab6c70bf863a3b044e54aee03f443f31a7edceb
RouVen-crp <rou_ven@163.com>
2023-11-18 21:28:42 +0800

    show vote status

--- RChain_Vote_Frontend/src/plugins/vuetify.js
+++ RChain_Vote_Frontend/src/plugins/vuetify.js
@@ -12,11 +12,17 @@ import 'vuetify/styles'
 
 // Composables
 import { createVuetify } from 'vuetify'
+import { aliases, fa } from 'vuetify/iconsets/fa'
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
+
   // icons: {
-  //   iconfont: 'md' || 'fa' 
+  //   defaultSet: 'fa',
+  //   aliases,
+  //   sets: {
+  //     fa,
+  //   },
   // },
   theme: {
     themes: {
